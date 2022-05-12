49a8bfb15f9c34c64d415247d1a37764822e9d84
R A <73193647+Mangust02@users.noreply.github.com>
2022-05-12 16:11:26 +0500

    module added

---
+++ crm/src/main/java/uz/mexe/crm/entity/Roles.java
@@ -0,0 +1,5 @@
+package uz.mexe.crm.entity;
+
+public enum Roles {
+    CONTENT_MANAGER, DESIGNER, COPYWRITER, ADMIN
+}
---
+++ crm/src/main/java/uz/mexe/crm/repository/RolesRepository.java
@@ -0,0 +1,6 @@
+package uz.mexe.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository {
+}
